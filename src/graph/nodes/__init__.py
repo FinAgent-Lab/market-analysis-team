cb99531f17c82505f6dac2aea10e07062751447e
Um Changyong <e7217@naver.com>
2025-03-20 10:57:31 +0000

    feat: RSS 피더 노드 및 관련 도구 추가, 의존성 업데이트

--- src/graph/nodes/__init__.py
+++ src/graph/nodes/__init__.py
@@ -1,11 +1,23 @@
 from src.graph.nodes.base import Node
 from src.graph.nodes.report_assistant import ReportAssistantNode
 from src.graph.nodes.supervisor import SupervisorNode
-from src.graph.nodes.naver_news_searcher import NaverNewsSearcherNode   
+from src.graph.nodes.naver_news_searcher import NaverNewsSearcherNode
+from src.graph.nodes.rss_feeder import (
+    ChosunRSSFeederNode,
+    WSJEconomyRSSFeederNode,
+    WSJMarketRSSFeederNode,
+)
 
 __all__ = [
     "Node",
-    "ReportAssistantNode",
     "SupervisorNode",
+    # Report Assistant
+    "ReportAssistantNode",
+    # Naver News Searcher
     "NaverNewsSearcherNode",
+    # RSS Feeder
+    "ChosunRSSFeederNode",
+    "WSJEconomyRSSFeederNode",
+    "WSJMarketRSSFeederNode",
+    "WSJTechRSSFeederNode",
 ]
