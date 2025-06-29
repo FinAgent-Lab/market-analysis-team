d4b104cad4fa2d85347f3f896130fd26157a44e3
Um Changyong <e7217@naver.com>
2025-06-29 11:46:00 +0000

    main.py에서 StockInfoNode 주석 처리, 종합 처리 기능 적용 시 주석 해제 예정
    #121

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def main(
     graph_builder.add_node(ChosunRSSFeederNode())
     graph_builder.add_node(WSJEconomyRSSFeederNode())
     graph_builder.add_node(WSJMarketRSSFeederNode())
-    graph_builder.add_node(StockInfoNode())  # TODO: 종합 처리 기능 적용 시 주석 해제
+    # graph_builder.add_node(StockInfoNode())  # TODO: 종합 처리 기능 적용 시 주석 해제
     graph_builder.add_node(CompanyFactsAnalyzerNode())
 
     # 한투 API 분석 에이전트 노드 주석 처리 (미국 주식 노드로 대체)
